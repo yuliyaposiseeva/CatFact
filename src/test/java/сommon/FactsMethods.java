735253873550af791435c460ad7879a31b72ca76
Юлия Посисеева <yuliyaposiseeva@mail.ru>
2020-07-20 15:25:27 +0000

    Значительно сократила код.

--- src/test/java/сommon/FactsMethods.java
+++ src/test/java/сommon/FactsMethods.java
@@ -1,89 +1,26 @@
 package сommon;
 
 import model.FactModel;
-import model.UserModel;
 
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class FactsMethods {
-    /**
-     * @param facts
-     * @return
-     *  Возвращаем пользователей из фактов
-     */
-
-    public static List<UserModel> getUsers(List<FactModel> facts) {
-        List<UserModel> users = facts
-                .stream()
-                .map(FactModel::getUser)
-                .collect(Collectors.toList());
-        return dropDuplicates(users);
-    }
 
     /**
-     * @param users
-     * @return
-     * Удаляем дубликаты.
+     * Метод определяет имя пользователя написавшего больше всего фактов.
+     *
+     * @param facts массив объектов класса FactsModel
+     * @return имя пользователя в формате строки
      */
-
-    private static List<UserModel> dropDuplicates(List<UserModel> users) {
-        return users
-                .stream()
-                .distinct()
-                .filter(Objects::nonNull)
-                .collect(Collectors.toList());
-    }
-
-    /**
-     * @param facts
-     * @param users
-     * @return
-     * Возвращаем массив массивов фактов и сортируем по убыванию.
-     */
-
-    public static List<List<FactModel>> sortFacts(List<FactModel> facts, List<UserModel> users) {
-        return compareFactsByUsers(sortAllFactsByUsers(facts, users));
-    }
-
-    /**
-     * @param facts
-     * @param users
-     * @return
-     * Создаем массив массивов фактов, каждый подмассив содержит факты только одного пользователя.
-     */
-
-    private static List<List<FactModel>> sortAllFactsByUsers(List<FactModel> facts, List<UserModel> users) {
-        return users.stream()
-                .map(user -> sortedFactsByUser(facts, user))
-                .collect(Collectors.toList());
-    }
-
-    /**
-     * @param facts
-     * @param user
-     * @return
-     * Метод возвращает только те факты, автором которых является переданный пользователь.
-     */
-
-    private static List<FactModel> sortedFactsByUser(List<FactModel> facts, UserModel user) {
-        return facts
-                .stream()
-                .filter(fact -> user.equals(fact.getUser()))
-                .collect(Collectors.toList());
-    }
-
-    /**
-     * @param facts
-     * @return
-     * Метод сортирует массивы, от большего к меньшему.
-     */
-
-    private static List<List<FactModel>> compareFactsByUsers(List<List<FactModel>> facts) {
-        return facts
-                .stream()
-                .sorted(((o1, o2) -> o2.size() - o1.size()))
-                .collect(Collectors.toList());
+    public static String getMostWrittenUser(List<FactModel> facts) {
+        return facts.
+                stream().
+                collect(Collectors.groupingBy(FactModel::getFullName, Collectors.counting())).
+                entrySet().
+                stream().
+                max(Map.Entry.comparingByValue()).
+                get().
+                getKey();
     }
 }
