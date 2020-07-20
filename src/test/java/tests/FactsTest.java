8df9ad0cb0b697660e94abaad06be0399570c565
Юлия Посисеева <yuliyaposiseeva@mail.ru>
2020-07-20 15:57:12 +0000

    Значительно сократила код.

--- src/test/java/tests/FactsTest.java
+++ src/test/java/tests/FactsTest.java
@@ -1,26 +1,16 @@
 package tests;
 
-import сommon.FactsMethods;
-import model.FactModel;
-import model.UserModel;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import requests.FactsRequest;
-
-import java.util.List;
+import сommon.FactsMethods;
 
 public class FactsTest {
 
-    /** Достаем пользователей из фактов.
-     * Сортируем факты по убыванию.
-     * Сортируем факты, автором которого является Kasimir Schulz.
-      */
-
-    @Test
+    @Test(description = "Тест проверяет,что больше всего фактов написал Kasimir Schultz")
     public void factsTest() {
-        List<FactModel> facts = FactsRequest.getFacts();
-        List<UserModel> users = FactsMethods.getUsers(facts);
-        List<List<FactModel>> sortedByUser = FactsMethods.sortFacts(facts, users);
-        Assert.assertEquals(sortedByUser.get(0).get(0).getUser().getName().getFullName(), "Kasimir Schulz");
+        String mostWrittenUser = FactsMethods.getMostWrittenUser(
+                FactsRequest.getFacts());
+        Assert.assertEquals(mostWrittenUser, "Kasimir Schulz");
     }
 }
